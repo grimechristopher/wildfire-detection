684b5e26859ec0a8b4ba9839f7907e83b513c204
grimechristopher <grimechristopher@gmail.com>
2021-02-03 14:29:51 -0800

    add comments

--- train_model.py
+++ train_model.py
@@ -10,6 +10,11 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import settings
 
 # Data preparation
+# Augmentation
+# Used increase the variation in the dataset. This allows the algorithm to have more samples to train the model with. 
+# https://www.tensorflow.org/tutorials/images/data_augmentation
+
+# Constructs an augmented data generator
 aug = ImageDataGenerator(
 		rescale=1./255, 
 		shear_range=0.2, 
@@ -18,26 +23,30 @@ aug = ImageDataGenerator(
 		)
 aug = ImageDataGenerator(rescale=1./255)
 
+# Create augmented data using the data generator that was just created
+# images is the data that will train the model and test is used to validate
 images = aug.flow_from_directory(settings.TRAIN_DIR, target_size=(settings.IMG_SIZE, settings.IMG_SIZE), batch_size=32, class_mode='binary')
 test = aug.flow_from_directory(settings.TRAIN_DIR, target_size=(settings.IMG_SIZE, settings.IMG_SIZE), batch_size=32, class_mode='binary')
 
-# CNN
+# Convolutional Neural Network (CNN)
 # A CNN consists of 3 main parts: Convolution, Pooling, Flattening
 
 # init CNN
 model = Sequential()
+
 # Convolution
 model.add(Conv2D(16, (7, 7), padding="same", input_shape=(settings.IMG_SIZE, settings.IMG_SIZE, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
-
 model.add(Conv2D(32, (3, 3), input_shape = (settings.IMG_SIZE, settings.IMG_SIZE, 3), activation='relu'))
+
 # Pooling
 # Pooling reduces overfitting because CNN will only get the features that are important for classification
 model.add(MaxPooling2D(pool_size=(2,2)))
-
 model.add(Conv2D(32, (3, 3), input_shape = (settings.IMG_SIZE, settings.IMG_SIZE, 3), activation='relu'))
+
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Conv2D(32, (3, 3), input_shape = (settings.IMG_SIZE, settings.IMG_SIZE, 3), activation='relu'))
+
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 # Flattening
