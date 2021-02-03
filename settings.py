ce6c5b1f794f503f0f3a6d105ff0ac683b4957d7
grimechristopher <grimechristopher@gmail.com>
2021-02-03 10:33:00 -0800

    Add descriptions

--- settings.py
+++ settings.py
@@ -1,16 +1,13 @@
-
-
 # Settings
 # Variable Settings
-IMG_SIZE = 256
-TRAIN_DIR = "images/train"
-EPOCHS = 10
-MODEL_PATH = "smoke_classifier.model"
+IMG_SIZE = 256  # the size images are resized before using to train models
+TRAIN_DIR = "images/train" #image training directory
+EPOCHS = 10 #The number of times the training script goes over the dataset
+MODEL_PATH = "smoke_classifier.model" #Location to save/load the model
 
-WIDTH = 1024 # Set images to be same width 
-#SLIDE_STEP_SIZE = 32 # How far each window moves 
-SLIDE_STEP_SIZE = 32 # How far each window moves 
+WIDTH = 1024 # Set images to be same width
+SLIDE_STEP_SIZE = 32 # How far each window moves when scaning image for smoke
 PYR_SCALE = 1.5 # ADD Pyramind scaling 
 MIN_SIZE = (300, 150) # Find the cameras aspect ratio...
-INPUT_SIZE = (IMG_SIZE, IMG_SIZE) # Size of the Classification dimensions
+INPUT_SIZE = (IMG_SIZE, IMG_SIZE) # Size of the Classification dimensions. The dimesions need to be the same as the image size the models used
 MODEL_PATH = "smoke_classifier.model" # USING IN BOTH
